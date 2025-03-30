37970e9a9eb400431fb6dcfce1b3319c9f474784
Shaik Rahim <rahim.shaik.job@gmail.com>
2025-03-30 09:41:38 +0530

    Build a setup for spring application

---
+++ src/test/java/com/dropresume/dropresume_backend/DropresumeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dropresume.dropresume_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropresumeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
