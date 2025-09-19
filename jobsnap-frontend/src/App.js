24c7ac755eaaa4470ed53e87443223049e47e2c9
Shaik Rahim <rahim.shaik.job@gmail.com>
2025-09-19 13:46:21 +0530

    changed files

--- jobsnap-frontend/src/App.js
+++ jobsnap-frontend/src/App.js
@@ -1,6 +1,11 @@
-import React from "react";
+import HomePage from "./pages/HomePage";
+import WhatItDoes from "./pages/WhatItDoes";
+import RegisterPage from "./pages/RegisterPage";
+import ContactPage from "./pages/ContactPage";
 import Navbar from "./components/Navbar";
+import LoginPage from "./pages/LoginPage";
 import LandingPage from "./pages/LandingPage";
+import { Routes, Route } from "react-router-dom";
 import "./styles/index.css"; // your CSS import
 
 function App() {
@@ -8,6 +13,13 @@ function App() {
     <>
       <Navbar />
       <LandingPage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/WhatItDoes" element={<WhatItDoes />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
     </>
   );
 }
