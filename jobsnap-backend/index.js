2d332885baefc724745864a55fd41be4d8760b9e
Shaik Rahim <rahim.shaik.job@gmail.com>
2025-12-24 00:16:57 +0530

    added files and started building jobsnap

--- jobsnap-backend/index.js
+++ jobsnap-backend/index.js
@@ -1,1 +1,14 @@
-console.log("Hello Js");
+const express = require("express");
+const jobsRoute = require("./routes/jobsRoute");
+const port = 5000;
+const app = express();
+const cors = require("cors");
+app.use(cors());
+
+//app.use("api", jobsRoute);
+
+app.get("/", (req, res) => {
+  res.send("Api is working...");
+});
+
+app.listen(port, () => console.log(`Server is running on ${port}...`));
